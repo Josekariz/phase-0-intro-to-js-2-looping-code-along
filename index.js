2bd5bcaa60b168149c196a59869e2d3a6b686b4a
Josekariz <joseph.macharia@student.moringaschool.com>
2022-08-15 22:29:09 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names, event) {
+  return names.map(
+    (name) => `Thank you, ${name}, for the wonderful ${event} gift!`
+  );
+}
+
+writeCards(names, "birthday");
+
+
+
+
+
+
+// countdown function
+function countDown() {
+  for (let i = 0; i < 11; i++) {
+    console.log(i);
+  }
+}
+countDown();
+
